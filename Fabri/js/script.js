ee2c427abe11d54952a3ee703f58ebcb02afc207
BCode Pro <162181896+AnteroTeobaldo@users.noreply.github.com>
2026-01-04 23:11:17 -0500

    Subir carpeta Fabri

--- Fabri/js/script.js
+++ Fabri/js/script.js
@@ -3,7 +3,7 @@
 // Configuración Matrix
 const LINE_COUNT = 30;
 const MATRIX_TEXT = "TE AMO ";
-const LOVE_CHARS = ["❤", "💖", "💕", "💞", "💝"];
+const LOVE_CHARS = ["❤", "❤️‍🩹", "💕", "🤍", "💗"];
 
 // Elementos interactivos
 const centro = document.querySelector('.centro');
