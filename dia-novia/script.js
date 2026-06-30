0ba6fe2d09e38b91777a43e46dab57d64ff22a38
BCode Pro <162181896+AnteroTeobaldo@users.noreply.github.com>
2026-06-29 21:37:18 -0500

    Clean up repository (system empty)

--- dia-novia/script.js
+++
@@ -1,5 +0,0 @@
-
-window.addEventListener('beforeunload', (event) => {
-       event.preventDefault();
-              event.returnValue = ''; // Chrome requiere un valor de retorno no vacío, aunque esté vacío
-                   });
